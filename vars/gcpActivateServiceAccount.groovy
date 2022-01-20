a56fa366bef643660f9d3a6d5a7ec64a9cc11b3d
Hari Sekhon <harisekhon@gmail.com>
2022-01-20 10:21:19 +0000

    added gcpActivateServiceAccount.groovy

---
+++ vars/gcpActivateServiceAccount.groovy
@@ -0,0 +1,34 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2021-09-01 12:50:03 +0100 (Wed, 01 Sep 2021)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/templates
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// Requires base64 encoded GCP_SERVICEACCOUNT_KEY environment variable to be set in environment{} section of Jenkinsfile, see top level Jenkinsfile template
+
+def call(timeoutMinutes=2){
+  echo "Activating GCP Service Account credential for Job '${env.JOB_NAME}' Build ${env.BUILD_ID} on ${env.JENKINS_URL}"
+  int timeoutSeconds = timeoutMinutes * 60
+  retry(2){
+    timeout(time: "$timeoutMinutes", unit: 'MINUTES') {
+      echo 'Activating GCP Service Account credential'
+      withEnv(["TIMEOUT_SECONDS=$timeoutSeconds"]) {
+        sh '''#!/bin/bash
+          set -euxo pipefail
+          echo "$GCP_SERVICEACCOUNT_KEY" | base64 --decode > credentials.json
+          gcloud auth activate-service-account --key-file=credentials.json
+          rm -f credentials.json
+        '''
+      }
+    }
+  }
+}
