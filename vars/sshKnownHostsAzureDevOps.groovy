53abfa91390408c56b7b7aea894db176ab25f354
Hari Sekhon <harisekhon@gmail.com>
2022-01-29 13:51:02 +0000

    added sshKnownHostsAzureDevOps.groovy

---
+++ vars/sshKnownHostsAzureDevOps.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+//
+//  Author: Hari Sekhon
+//  Date: 2022-01-28 16:10:36 +0000 (Fri, 28 Jan 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// Loads Azure DevOps SSH Known Hosts
+//
+//    ssh-keyscan ssh.dev.azure.com
+
+def call() {
+  sshKnownHosts("""
+# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
+# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
+# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
+ssh.dev.azure.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7Hr1oTWqNqOlzGJOfGJ4NakVyIzf1rXYd4d7wo6jBlkLvCA4odBlL0mDUyZ0/QUfTTqeu+tm22gOsv+VrVTMk6vwRU75gY/y9ut5Mb3bR5BV58dKXyq9A9UeB5Cakehn5Zgm6x1mKoVyf+FFn26iYqXJRgzIZZcZ5V6hrE0Qg39kZm4az48o0AUbf6Sp4SLdvnuMa2sVNwHBboS7EJkm57XQPVU3/QpyNLHbWDdzwtrlS+ez30S3AdYhLKEOxAG8weOnyrtLJAUen9mTkol8oII1edf7mWWbWVf0nBmly21+nZcmCTISQBtdcyPaEno7fFQMDD26/s0lfKob4Kw8H
+"""
+  )
+}
