9f396e474ad59a0098c96b932a0b7cbd69951a1b
Hari Sekhon <harisekhon@gmail.com>
2022-01-29 14:39:02 +0000

    updated sshKnownHosts.groovy

--- vars/sshKnownHosts.groovy
+++ vars/sshKnownHosts.groovy
@@ -20,6 +20,9 @@
 
 def call(known_hosts='', name='') {
   withEnv(["SSH_KNOWN_HOSTS=$known_hosts"]){
+    when {
+      not { environment name: 'SSH_KNOWN_HOSTS', value: '' }
+    }
     sh (
       label: "Adding SSH Known Hosts: $name",
       script: '''#!/bin/bash
