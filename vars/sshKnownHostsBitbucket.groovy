eeebc1eb71f2b1f52901ddb7a3378872518080a5
Hari Sekhon <harisekhon@gmail.com>
2022-01-29 13:52:14 +0000

    added sshKnownHostsBitbucket.groovy

---
+++ vars/sshKnownHostsBitbucket.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+//
+//  Author: Hari Sekhon
+//  Date: 2022-01-28 16:10:36 +0000 (Fri, 28 Jan 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// Loads Bitbucket.org SSH Known Hosts
+//
+//    ssh-keyscan bitbucket.org
+
+def call() {
+  sshKnownHosts("""
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 43c1f25558ba
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 5325bd181e2d
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 75cc8ff6b5b2
+bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw==
+"""
+  )
+}
