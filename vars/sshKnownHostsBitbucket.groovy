989ddb809d66508525c848f6406093d1dfc514b0
Hari Sekhon <harisekhon@gmail.com>
2022-01-29 14:01:56 +0000

    updated sshKnownHostsBitbucket.groovy

--- vars/sshKnownHostsBitbucket.groovy
+++ vars/sshKnownHostsBitbucket.groovy
@@ -20,9 +20,9 @@
 
 def call() {
   sshKnownHosts("""
-# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 43c1f25558ba
-# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 5325bd181e2d
-# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 75cc8ff6b5b2
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e d387e8eb814e
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e 29a8875ef620
+# bitbucket.org:22 SSH-2.0-conker_3aee7f2e9e c6f6c13a1084
 bitbucket.org ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw==
 """
   )
