338e0ec0c89a270f1cfc442aaea0cd80b8bfc3bc
Hari Sekhon <harisekhon@gmail.com>
2022-01-29 14:02:32 +0000

    updated sshKnownHostsAzureDevOps.groovy

--- vars/sshKnownHostsAzureDevOps.groovy
+++ vars/sshKnownHostsAzureDevOps.groovy
@@ -21,9 +21,9 @@
 def call() {
   sshKnownHosts("""
 # ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
-# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
-# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
 ssh.dev.azure.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC7Hr1oTWqNqOlzGJOfGJ4NakVyIzf1rXYd4d7wo6jBlkLvCA4odBlL0mDUyZ0/QUfTTqeu+tm22gOsv+VrVTMk6vwRU75gY/y9ut5Mb3bR5BV58dKXyq9A9UeB5Cakehn5Zgm6x1mKoVyf+FFn26iYqXJRgzIZZcZ5V6hrE0Qg39kZm4az48o0AUbf6Sp4SLdvnuMa2sVNwHBboS7EJkm57XQPVU3/QpyNLHbWDdzwtrlS+ez30S3AdYhLKEOxAG8weOnyrtLJAUen9mTkol8oII1edf7mWWbWVf0nBmly21+nZcmCTISQBtdcyPaEno7fFQMDD26/s0lfKob4Kw8H
+# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
+# ssh.dev.azure.com:22 SSH-2.0-SSHBlackbox.10
 """
   )
 }
