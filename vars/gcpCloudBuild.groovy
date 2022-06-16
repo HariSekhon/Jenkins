cb1f357c38168709a7a4441f123de44e18910c23
Hari Sekhon <harisekhon@gmail.com>
2022-06-17 00:07:18 +0100

    updated gcpCloudBuild.groovy

--- vars/gcpCloudBuild.groovy
+++ vars/gcpCloudBuild.groovy
@@ -17,7 +17,7 @@
 //
 // Example call:
 //
-//    gcpCloudBuild('--project="$GCR_PROJECT" --substitutions="_REGISTRY=$GCR_REGISTRY,_IMAGE_VERSION=$GIT_COMMIT,_GIT_BRANCH=${GIT_BRANCH##*/}"')
+//    gcpCloudBuild(args: '--project="$GCR_PROJECT" --substitutions="_REGISTRY=$GCR_REGISTRY,_IMAGE_VERSION=$GIT_COMMIT,_GIT_BRANCH=${GIT_BRANCH##*/}"', timeoutMinutes: 30)
 //
 // If the following are set then will check GCR for an existing image to skip building entirely
 //
