3719386c5ce5527b85b8a8106b50360ecfb7e5a4
Hari Sekhon <harisekhon@gmail.com>
2022-06-21 13:49:23 +0100

    updated installBinary.groovy

--- vars/installBinary.groovy
+++ vars/installBinary.groovy
@@ -3,7 +3,7 @@
 //  Author: Hari Sekhon
 //  Date: 2022-06-21 13:12:02 +0100 (Tue, 21 Jun 2022)
 //
-//  vim:ts=4:sts=4:sw=4:et
+//  vim:ts=2:sts=2:sw=4:et
 //
 //  https://github.com/HariSekhon/Jenkins
 //
@@ -19,23 +19,23 @@
 // ========================================================================== //
 
 def call(Map args = [url: '', binary: '']) {
-    withEnv(["URL=${args.get(url, '')}", "BINARY=${args.get(binary, '')}"]){
-        sh '''
-            set -eux
+  withEnv(["URL=${args.get(url, '')}", "BINARY=${args.get(binary, '')}"]){
+    sh '''
+      set -eux
 
-            mkdir -p ~/bin
+      mkdir -p ~/bin
 
-            cd ~/bin
+      cd ~/bin
 
-            export PATH="$PATH:$HOME/bin:$HOME/bin/bash-tools"
-            export NO_MAKE=1
+      export PATH="$PATH:$HOME/bin:$HOME/bin/bash-tools"
+      export NO_MAKE=1
 
-            # downloads the DevOps Bash tools repo which contains install_binary.sh
-            curl -L https://git.io/bash-bootstrap | sh
+      # downloads the DevOps Bash tools repo which contains install_binary.sh
+      curl -L https://git.io/bash-bootstrap | sh
 
-            # this install_binary.sh script has lots of well tested logic we don't want to duplicate here
-            #
-            bash-tools/install_binary.sh "$URL" "$BINARY"
-        '''
-    }
+      # this install_binary.sh script has lots of well tested logic we don't want to duplicate here
+      #
+      bash-tools/install_binary.sh "$URL" "$BINARY"
+    '''
+  }
 }
