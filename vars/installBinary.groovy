32d03d2ef326039a449b9aeb2a6687a3da7b711a
Hari Sekhon <harisekhon@gmail.com>
2022-06-21 13:28:15 +0100

    updated installBinary.groovy

--- vars/installBinary.groovy
+++ vars/installBinary.groovy
@@ -22,11 +22,19 @@ def call(Map args = [url: '', binary: '']) {
     withEnv(["URL=${args.get(url, '')}", "BINARY=${args.get(binary, '')}"]){
         sh '''
             set -eux
+
             mkdir -p ~/bin
+
             cd ~/bin
+
             export PATH="$PATH:$HOME/bin:$HOME/bin/bash-tools"
             export NO_MAKE=1
+
+            # downloads the DevOps Bash tools repo which contains install_binary.sh
             curl -L https://git.io/bash-bootstrap | sh
+
+            # this install_binary.sh script has lots of well tested logic we don't want to duplicate here
+            #
             bash-tools/install_binary.sh "$URL" "$BINARY"
         '''
     }
