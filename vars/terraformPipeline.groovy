78ddca9cfaf37217aadc98605be2f701f5e1b9e9
Hari Sekhon <harisekhon@gmail.com>
2022-06-21 14:14:34 +0100

    updated terraformPipeline.groovy

--- vars/terraformPipeline.groovy
+++ vars/terraformPipeline.groovy
@@ -55,7 +55,7 @@ def call(Map args = [
 
   pipeline {
 
-    agent args.get(agent, any)
+    agent ${args.get(agent, any)}
 
     // XXX: better to set jenkins-pod.yaml in the repo to a container with all the tooling needed
     //      using terraform's official docker image seemed smart for caching but it lacks the cloud auth tooling to be effective
