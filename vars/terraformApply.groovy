247dfb8ef6bc0c0d8b69b2270db95214de81cd74
Hari Sekhon <harisekhon@gmail.com>
2022-06-21 18:07:04 +0100

    updated terraformApply.groovy

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -13,7 +13,9 @@
 //  https://www.linkedin.com/in/HariSekhon
 //
 
-// $APP and $ENVIRONMENT must be set in pipeline to ensure separate locking
+// ========================================================================== //
+//                         T e r r a f o r m   A p p l y
+// ========================================================================== //
 
 def call(timeoutMinutes=30){
   String terraformDir = env.TERRAFORM_DIR ?: '.'
