14990cc995f0fa9a529f5477d173f1be8555d26c
Hari Sekhon <harisekhon@gmail.com>
2022-06-21 20:59:00 +0100

    updated terraformInit.groovy

--- vars/terraformInit.groovy
+++ vars/terraformInit.groovy
@@ -32,31 +32,32 @@ def call(timeoutMinutes=10){
         // let's check we have the login creds we think we should (checking up on the logins() function you should have called earlier)
         printAuth()
 
-        // terraform workspace is not supported if using Terraform Cloud
-        // TF_WORKSPACE overrides 'terraform workspace select'
-        //
-        // terraform docker image doesn't have bash
-        //sh '''#/usr/bin/env bash -euxo pipefail
-        //
         // org.jenkinsci.plugins.scriptsecurity.sandbox.RejectedAccessException: Scripts not permitted to use staticMethod java.lang.System getenv java.lang.String
         //dir(System.getenv("TERRAFORM_DIR") ?: ".") {
         dir(env.TERRAFORM_DIR ?: ".") {
+
+          // terraform workspace is not supported if using Terraform Cloud
+          // TF_WORKSPACE overrides 'terraform workspace select'
+
+          // terraform docker image doesn't have bash
+          //sh '''#/usr/bin/env bash -euxo pipefail
+
           echo 'Terraform Workspace Select'
           sh label: 'Workspace Select',
-             script: '''#/bin/sh -eux
-                       if [ -n "$TF_WORKSPACE" ]; then
-                           terraform workspace new "$TF_WORKSPACE" || echo "Workspace '$TF_WORKSPACE' already exists or using Terraform Cloud as a backend"
-                           #terraform workspace select "$TF_WORKSPACE"  # TF_WORKSPACE takes precedence over this select
-                       fi
-                      '''
+             script: '''
+               set -eux
+               if [ -n "$TF_WORKSPACE" ]; then
+                   terraform workspace new "$TF_WORKSPACE" || echo "Workspace '$TF_WORKSPACE' already exists or using Terraform Cloud as a backend"
+                   #terraform workspace select "$TF_WORKSPACE"  # TF_WORKSPACE takes precedence over this select
+               fi
+             '''
+
           echo "$label"
-          sh (
-            label: "$label",
-            script: '''
-              terraform init -input=false
-            '''
-            // -backend-config "bucket=$ACCOUNT-$PROJECT-terraform" -backend-config "key=${ENV}-${PRODUCT}/${COMPONENT}/state.tf"
-          )
+          sh label: "$label",
+             script: '''
+               terraform init -input=false
+             '''
+             // -backend-config "bucket=$ACCOUNT-$PROJECT-terraform" -backend-config "key=${ENV}-${PRODUCT}/${COMPONENT}/state.tf"
         }
       }
     }
