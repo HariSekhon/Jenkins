d26813194dd8714779356e337f53cdbd34ec4f1d
Hari Sekhon <harisekhon@gmail.com>
2022-06-27 13:46:10 +0100

    updated installPackages.groovy

--- vars/installPackages.groovy
+++ vars/installPackages.groovy
@@ -3,7 +3,7 @@
 //  Author: Hari Sekhon
 //  Date: 2022-06-27 13:23:20 +0100 (Mon, 27 Jun 2022)
 //
-//  vim:ts=4:sts=4:sw=4:et
+//  vim:ts=2:sts=2:sw=2:et
 //
 //  https://github.com/HariSekhon/Jenkins
 //
@@ -21,25 +21,29 @@
 // Adapted from the more advanced DevOps Bash tools repo's install_packages.sh scripts
 
 def call(packages=[]){
-    sh """
+  timeout(time: 5, unit: 'MINUTES') {
+    withEnv(["PACKAGES=${packages.join(' ')}"]){
+      sh '''
         set -eux
 
         export DEBIAN_FRONTEND=noninteractive
 
         sudo=""
         # adapted from DevOps Bash tools lib/utils.sh am_root() function
-        if ! [ "\${EUID:-\${UID:-\$(id -u)}}" -eq 0 ]; then
-            sudo=sudo
+        if ! [ "${EUID:-${UID:-$(id -u)}}" -eq 0 ]; then
+          sudo=sudo
         fi
 
         if type -P apt-get >/dev/null; then
-            \$sudo apt-get update
-            \$sudo apt-get install -y ${packages.join(' ')}
+          $sudo apt-get update
+          $sudo apt-get install -y $PACKAGES
         elif type -P apk >/dev/null; then
-            \$sudo apk update
-            \$sudo apk add ${packages.join(' ')}
+          $sudo apk update
+          $sudo apk add $PACKAGES
         elif type -P yum >/dev/null; then
-            \$sudo yum install -y ${packages.join(' ')}
+          $sudo yum install -y $PACKAGES
         fi
-    """
+      '''
+    }
+  }
 }
