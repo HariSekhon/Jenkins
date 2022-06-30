806c5f0652c39b9edb266f30d1c364f6396bdfde
Hari Sekhon <harisekhon@gmail.com>
2022-06-30 19:15:41 +0100

    added downloadJenkinsCLI.groovy

---
+++ vars/downloadJenkinsCLI.groovy
@@ -0,0 +1,46 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2022-06-30 18:03:25 +0100 (Thu, 30 Jun 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                Download Jenkins CLI from current Jenkins Master
+// ========================================================================== //
+
+// Downloads Terraform binary to $HOME/bin if run as a user, or /usr/local/bin if run as root
+
+// Adapted from DevOps Bash Tools jenkins_cli.sh, install_binary.sh, install_packages.sh and lib/utils.sh
+
+def call() {
+  timeout(time: 5, unit: 'MINUTES') {
+    sh (
+      label: 'Download Jenkins CLI',
+      script: '''
+        set -eux
+
+        # adapted from DevOps Bash tools lib/utils.sh am_root() function
+        if [ "${EUID:-${UID:-$(id -u)}}" -eq 0 ]; then
+          destination=/usr/local/bin
+        else
+          destination=~/bin
+        fi
+
+        url="$JENKINS_URL/jnlpJars/jenkins-cli.jar"
+
+        curl -sSLf -o jenkins-cli.jar "$url"
+
+        mv -v jenkins-cli.jar "$destination/"
+      '''
+    )
+  }
+}
