5f01014e9cc4c585c77b3214a2981fa9fa028996
Hari Sekhon <harisekhon@gmail.com>
2022-07-01 11:09:48 +0100

    updated jenkinsBackupJobConfigsPipeline.groovy

--- vars/jenkinsBackupJobConfigsPipeline.groovy
+++ vars/jenkinsBackupJobConfigsPipeline.groovy
@@ -148,7 +148,7 @@ def call(Map args = [
                 label: 'Version',
                 script: '''
                   set -eux
-                  java -jar ~/jenkins-cli.jar ${JENKINS_CLI_ARGS:-} version
+                  java -jar ~/bin/jenkins-cli.jar ${JENKINS_CLI_ARGS:-} version
                 '''
               )
             }
