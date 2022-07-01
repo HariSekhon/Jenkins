51217da6ce4001b91747474cfc9b9166fa6ba04a
Hari Sekhon <harisekhon@gmail.com>
2022-07-01 10:47:08 +0100

    updated installBinary.groovy

--- vars/installBinary.groovy
+++ vars/installBinary.groovy
@@ -17,40 +17,120 @@
 //                          I n s t a l l    B i n a r y
 // ========================================================================== //
 
-// Downloads a URL, extracts the given binary from the tarball/zip and copies it to $HOME/bin if run as a user, or /usr/local/bin if run as root
+// Downloads a URL, extracts the given binary from the tarball/zip and copies it to $HOME/bin
 
-def call(Map args = [url: '', binary: '']) {
-  withEnv(["URL=${args.url}", "BINARY=${args.binary}"]){
-    sh '''
-      set -eux
+// Adapted from the more advanced DevOps Bash tools repo's install_binary.sh and supporting scripts and lib/utils.sh
+// TODO: could make this native Groovy instead of sh script
 
-      if [ -z "$URL" ]; then
-        echo "No URL passed to installBinary()"
-        exit 1
-      fi
-      if [ -z "$BINARY" ]; then
-        echo "No binary name passed to installBinary()"
-        exit 1
-      fi
+// you may need to call this to ensure the prerequisite commands for curl / tar / unzip are available:
+//
+//    installPackages(['curl', 'tar', 'unzip'])
 
-      mkdir -p ~/bin
+// binary should be path to the binary after being unpacked, including any intermediate directory paths in an unpacked tarball or zip package
+def call(Map args = [url: '', binary: '', overwrite: false, timeout: 15, timeoutUnits: 'MINUTES']) {
+  timeout(time: args.get('timeout', 15),
+          unit: args.get('timeoutUnits', 'MINUTES') ) {
+    withEnv([
+        "URL=${args.get('url', '')}",
+        "BINARY=${args.get('binary', '')}",
+        "OVERWRITE=${args.get('binary', false)}"
+      ]
+    ){
+      sh (
+        label: "Install Binary '${env.BINARY.tokenize('/')[-1]}' from $URL",
+        script: '''
+          set -eux
 
-      cd ~/bin
+          if [ -z "$URL" ]; then
+            echo "No URL passed to installBinary()"
+            exit 1
+          fi
+          if [ -z "$BINARY" ]; then
+            echo "No binary name passed to installBinary()"
+            exit 1
+          fi
 
-      export PATH="$PATH:$HOME/bin:$HOME/bin/bash-tools"
+          os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+          arch="$(uname -m)"
+          if [ "$arch" = x86_64 ]; then
+              arch=amd64  # files are conventionally named amd64 not x86_64
+          fi
 
-      if [ -d bash-tools ]; then
-        # pushd not available in sh
-        cd bash-tools
-        git pull
-        cd ..
-      else
-        git clone https://github.com/HariSekhon/DevOps-Bash-tools bash-tools
-      fi
+          URL="${URL//\\{os\\}/$os}"
+          URL="${URL//\\{arch\\}/$arch}"
 
-      # this install_binary.sh script has lots of well tested logic we don't want to duplicate here
-      #
-      bash-tools/install_binary.sh "$URL" "$BINARY"
-    '''
+          destination=~/bin
+
+          mkdir -p -v "$destination"
+
+          cd "$destination"
+
+          if [ -f "BINARY" ]; then
+            if [ "$OVERWRITE" != true ]; then
+              echo "$BINARY already present, skipping"
+              exit 0
+            fi
+          fi
+
+          package="${URL##*/}"
+          tmp="/tmp/installBinary.$$"
+          download_file="$tmp/$package"
+
+          mkdir -p -v "$tmp"
+
+          # copied from DevOps Bash tools lib/utils.sh
+          export TRAP_SIGNALS="INT QUIT TRAP ABRT TERM EXIT"
+          trap "rm -f '$download_file'" $TRAP_SIGNALS
+
+          cd "$tmp"
+
+          echo "Downloading: $URL"
+          curl -sSLf -o "$download_file" "$URL"
+
+          # adapted from has_tarball_extension() in lib/utils.sh
+          if [[ "$package" =~ \\.(tgz|tar\\.gz|tbz|tar\\.bz2)$ ]]; then
+              echo "Extracting tarball package"
+              if [[ "$package" =~ \\.(tgz|tar\\.gz)$ ]]; then
+                  tar xvzf "$download_file"
+              elif [[ "$package" =~ \\.(tbz|tar\\.bz2)$ ]]; then
+                  tar xvjf "$download_file"
+              fi
+              if ! [ -f "$BINARY" ]; then
+                  echo "Failed to find binary '$BINARY' in unpacked '$download_file' - is the given binary filename / path correct?"
+                  exit 1
+              fi
+              download_file="$BINARY"
+              echo
+          elif [[ "$package" =~ \\.zip$ ]]; then
+              echo "Extracting zip package"
+              unzip -o "$download_file"
+              download_file="$BINARY"
+          fi
+
+          echo "Setting executable: $download_file"
+          chmod +x "$download_file"
+          echo
+
+          destination="$destination/${download_file##*/}"
+          destination="${destination%%.$$}"
+          # if there are any -darwin-amd64 or -amd64-darwin suffixes remove them either way around (this is why $os is stripped before and after)
+          destination="${destination%%-$os}"
+          destination="${destination%%_$os}"
+          destination="${destination%%-$arch}"
+          destination="${destination%%_$arch}"
+          destination="${destination%%-$os}"
+          destination="${destination%%_$os}"
+
+          install_path="${destination%/*}"
+
+          # common alias mv='mv -i' would force a prompt we don't want, even with -f
+          unalias mv >/dev/null 2>&1 || :
+          mv -fv "$download_file" "$destination"
+          echo
+
+          echo "Installation complete"
+        '''
+      )
+    }
   }
 }
