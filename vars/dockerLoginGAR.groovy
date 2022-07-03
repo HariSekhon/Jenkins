d246c4f3af3e5083bb887eaa61fea406bf48cf8d
Hari Sekhon <harisekhon@gmail.com>
2022-07-03 19:57:57 +0100

    updated dockerLoginGAR.groovy

--- vars/dockerLoginGAR.groovy
+++ vars/dockerLoginGAR.groovy
@@ -17,33 +17,31 @@
 //                    Docker Login to Google Artifact Registry
 // ========================================================================== //
 
-// must be called after gcpActivateServiceAccount.groovy and have GCloud SDK in the calling environment
-// or will fall back to attempting a direct Docker login if GCP_SERVICEACCOUNT_KEY is present in the environment
-//
-// must have $GAR_REGISTRY set in the environment to know which registries to log in to (can be a comma separated list of registries)
+// pass GCP_SERVICE_ACCOUNT key and GAR_REGISTRY as the first and second args or it'll look for them in the environment
+
+// must be called after gcpActivateServiceAccount.groovy
+// must have GCloud SDK in the calling environment or will fall back to attempting a direct Docker login
 //
 // GAR registries list can be obtained via:
 //
 //    gcloud artifacts locations list
 
-def call() {
-  // configures docker config with a token
-  sh '''
-    set -eux
-
-    if command -v gcloud &>/dev/null; then
-      gcloud auth configure-docker "$GAR_REGISTRY"
-    else
-      echo "GCloud SDK is not installed, attempting to login with docker directly" >&2
-      if [ -z "${GAR_REGISTRY:-}" ]; then
-        echo "GAR_REGISTRY environment variable not set!" >&2
-        exit 1
-      fi
-      if [ -z "${GCP_SERVICEACCOUNT_KEY:-}" ]; then
-        echo "GCP_SERVICEACCOUNT_KEY environment variable not set!" >&2
-        exit 1
-      fi
-      docker login "$GAR_REGISTRY" -u _json_key -p "$(echo "$GCP_SERVICEACCOUNT_KEY" | base64 --decode)"
-    fi
-  '''
+def call(key="$GCP_SERVICEACCOUNT_KEY", registry="$GAR_REGISTRY") {
+  withEnv(["GCP_SERVICEACCOUNT_KEY=$key", "GAR_REGISTRY=$registry"]){
+    script {
+      if(isCommandAvailable('gcloud')){
+        echo 'Using GCloud SDK to configure Docker'
+        gcloud auth configure-docker "$GAR_REGISTRY"
+      } else {
+        echo 'GCloud SDK is not installed, attempting to login with docker directly'
+        if(!env.GAR_REGISTRY){
+          error('GAR_REGISTRY environment variable not set!')
+        }
+        if(!env.GCP_SERVICEACCOUNT_KEY){
+          error('GCP_SERVICEACCOUNT_KEY environment variable not set!')
+        }
+        dockerLogin('_json_key', env.GCP_SERVICEACCOUNT_KEY.bytes.decodeBase64().toString(), env.GAR_REGISTRY)
+      }
+    }
+  }
 }
