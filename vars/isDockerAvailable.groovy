fa27df1197ddc62e7b21779f67e02572620ff47d
Hari Sekhon <harisekhon@gmail.com>
2022-07-03 18:49:19 +0100

    added isDockerAvailable.groovy

---
+++ vars/isDockerAvailable.groovy
@@ -0,0 +1,30 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2022-07-03 18:47:04 +0100 (Sun, 03 Jul 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  Liceese: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                     I s   D o c k e r   A v a i l a b l e
+// ========================================================================== //
+
+// returns Boolean if 'docker' command is found in the $PATH
+//
+// XXX: could extend to check that Docker server is responding too
+
+def call(){
+  sh(
+    label: 'Check if Docker command is available',
+    returnStatus: true,
+    script: 'command -v docker'
+  )
+}
