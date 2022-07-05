2cbe145f2ceb2fbbe7d002a05e9ba71d0bc6c2ff
Hari Sekhon <harisekhon@gmail.com>
2022-07-05 17:03:01 +0100

    updated dockerLoginGHCR.groovy

--- vars/dockerLoginGHCR.groovy
+++ vars/dockerLoginGHCR.groovy
@@ -20,7 +20,7 @@
 // GITHUB_USER and GH_TOKEN / GITHUB_TOKEN must be passed as args or set in the calling environment
 
 def call(user='', token='') {
-  user = user ?: env.get('GITHUB_USER', error('dockerLoginGHCR: user not specified and GITHUB_USER not set in the environment'))
-  token = token ?: env.get('GH_TOKEN', env.get('GITHUB_TOKEN', error('dockerLoginGHCR: token not specified and GH_TOKEN / GITHUB_TOKEN not set in the environment')))
+  user = user ?: env.GITHUB_USER ?: error('dockerLoginGHCR: user not specified and GITHUB_USER not set in the environment')
+  token = token ?: env.GH_TOKEN ?: env.GITHUB_TOKEN ?: error('dockerLoginGHCR: token not specified and GH_TOKEN / GITHUB_TOKEN not set in the environment')
   dockerLogin(user, token, 'ghcr.io')
 }
