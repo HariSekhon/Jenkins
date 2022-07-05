5f51525e12c8d5c027a2012968135d692bbf5de6
Hari Sekhon <harisekhon@gmail.com>
2022-07-05 11:10:58 +0100

    updated dockerLoginGCR.groovy

--- vars/dockerLoginGCR.groovy
+++ vars/dockerLoginGCR.groovy
@@ -23,8 +23,8 @@
 // must have GCloud SDK in the calling environment or will fall back to attempting a direct Docker login
 
 def call(key='', registry='') {
-  key = key ?: env.get("GCP_SERVICEACCOUNT_KEY", error('dockerLoginGCR: key not specified and GCP_SERVICEACCOUNT_KEY not set in the environment'))
-  registry = registry ?: env.get("GCR_REGISTRY", error('dockerLoginGCR: registry not specified and GCR_REGISTRY not set in the environment'))
+  key = key ?: env.get('GCP_SERVICEACCOUNT_KEY', error('dockerLoginGCR: key not specified and GCP_SERVICEACCOUNT_KEY not set in the environment'))
+  registry = registry ?: env.get('GCR_REGISTRY', error('dockerLoginGCR: registry not specified and GCR_REGISTRY not set in the environment'))
   withEnv(["GCP_SERVICEACCOUNT_KEY=$key", "GCR_REGISTRY=$registry"]){
     script {
       if(isCommandAvailable('gcloud')){
