58d48f162c146892fafd9d414ded6c754b058b73
Hari Sekhon <harisekhon@gmail.com>
2022-07-20 17:18:31 +0100

    updated parameterizedPipeline.groovy

--- vars/parameterizedPipeline.groovy
+++ vars/parameterizedPipeline.groovy
@@ -21,67 +21,76 @@
 
 def call (project, environ, credential) {
 
-    pipeline {
+  pipeline {
 
-      agent {
-        kubernetes {
-          defaultContainer 'gcloud-sdk'
-          yamlFile "ci/jenkins-pod.yaml"
+    agent {
+      kubernetes {
+        defaultContainer 'gcloud-sdk'
+        yamlFile "ci/jenkins-pod.yaml"
+      }
+    }
+
+    environment {
+      CLOUDSDK_CORE_PROJECT = "$project"
+      ENVIRONMENT = "${environ}"
+      GCP_SERVICEACCOUNT_KEY = credentials("$credential")
+    }
+
+    options {
+      buildDiscarder(logRotator(numToKeepStr: '100'))
+      timestamps()
+      timeout(time: 1, unit: 'HOURS')
+    }
+
+    stages {
+
+      stage('Build') {
+        steps {
+          milestone ordinal: null, label: "Milestone: Build"
+          echo "Running ${env.JOB_NAME} Build ${env.BUILD_ID} on ${env.JENKINS_URL}"
+          timeout(time: 1, unit: 'MINUTES') {
+            sh script: 'env | sort', label: 'Environment'
+          }
+          retry(2){
+            timeout(time: 40, unit: 'MINUTES') {
+              // script from DevOps Bash tools repo
+              // external script needs to exist in the source repo, not the shared library repo
+              //
+              // obsoleted by gcpCloudBuild.groovy which doesn't need a script to be available in the user repo
+              sh 'gcp_ci_build.sh'
+            }
+          }
         }
       }
 
-      environment {
-        CLOUDSDK_CORE_PROJECT = "$project"
-        ENVIRONMENT = "${environ}"
-        GCP_SERVICEACCOUNT_KEY = credentials("$credential")
-      }
-
-      options {
-        buildDiscarder(logRotator(numToKeepStr: '100'))
-        timestamps()
-        timeout(time: 1, unit: 'HOURS')
-      }
-
-      stages {
-
-        stage('Build') {
-          steps {
-            milestone ordinal: null, label: "Milestone: Build"
-            echo "Running ${env.JOB_NAME} Build ${env.BUILD_ID} on ${env.JENKINS_URL}"
-            timeout(time: 1, unit: 'MINUTES') {
-              sh script: 'env | sort', label: 'Environment'
-            }
+      stage('Deploy') {
+        steps {
+          milestone ordinal: null, label: "Milestone: Deploy"
+          lock(resource: "Deploy K8s Apps - '" + "${environ}".capitalize() + "' Environment", inversePrecedence: true){
             retry(2){
-              timeout(time: 40, unit: 'MINUTES') {
+              timeout(time: 20, unit: 'MINUTES') {
                 // script from DevOps Bash tools repo
                 // external script needs to exist in the source repo, not the shared library repo
                 //
-                // obsoleted by gcpCloudBuild.groovy which doesn't need a script to be available in the user repo
-                sh 'gcp_ci_build.sh'
+                // obsoleted by argoDeploy.groovy which doesn't need a script to be available in the user repo
+                sh 'gcp_ci_k8s_deploy.sh'
               }
             }
           }
         }
-
-        stage('Deploy') {
-          steps {
-            milestone ordinal: null, label: "Milestone: Deploy"
-            lock(resource: "Deploy K8s Apps - '" + "${environ}".capitalize() + "' Environment", inversePrecedence: true){
-              retry(2){
-                timeout(time: 20, unit: 'MINUTES') {
-                  // script from DevOps Bash tools repo
-                  // external script needs to exist in the source repo, not the shared library repo
-                  //
-                  // obsoleted by argoDeploy.groovy which doesn't need a script to be available in the user repo
-                  sh 'gcp_ci_k8s_deploy.sh'
-                }
-              }
-            }
-          }
-        }
-
       }
 
     }
+
+    post {
+      failure {
+        Notify()
+      }
+      fixed {
+        Notify()
+      }
+    }
+
+  }
 
 }
