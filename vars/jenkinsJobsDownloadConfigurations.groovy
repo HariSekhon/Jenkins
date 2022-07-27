5a2ec4b87b64732e3509efe088aad26248a31014
Hari Sekhon <harisekhon@gmail.com>
2022-07-27 18:34:24 +0100

    updated jenkinsJobsDownloadConfigurations.groovy

--- vars/jenkinsJobsDownloadConfigurations.groovy
+++ vars/jenkinsJobsDownloadConfigurations.groovy
@@ -44,7 +44,11 @@ def call(Map args = [ jobs: [], excludeJobs: [] ]) {
   List jobs = args.jobs ?: []
   List excludedJobs = args.excludeJobs ?: defaultExcludedJobs
 
-  jenkinsCliJar = env.get('JENKINS_CLI_JAR') ?: "$HOME/bin/jenkins-cli.jar"
+  if(env.JENKINS_CLI_JAR){
+    jenkinsCliJar = env.JENKINS_CLI_JAR
+  } else {
+    jenkinsCliJar = "$HOME/bin/jenkins-cli.jar"
+  }
   jenkinsCliJarFile = new File(jenkinsCliJar)
   if(! jenkinsCliJarFile.exists() ){
     echo "$jenkinsCliJar not found, downloading..."
