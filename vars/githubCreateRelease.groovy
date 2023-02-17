61c9d7bce884ab4b62a95901edfa98cd57821123
Hari Sekhon <harisekhon@gmail.com>
2023-02-17 19:40:48 +0000

    added githubCreateRelease.groovy

---
+++ vars/githubCreateRelease.groovy
@@ -0,0 +1,66 @@
+#!/usr/bin/env groovy
+//
+//  Author: Hari Sekhon
+//  Date: 2023-02-17 19:16:24 +0000 (Fri, 17 Feb 2023)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                   G i t H u b   C r e a t e   R e l e a s e
+// ========================================================================== //
+
+// Creates a GitHub Release Tag of the given argument
+//
+// If no release tag is given, tries to auto-determine the next increment from the current latest release tag using adjacent function githubNextRelease.groovy - see there for more details
+//
+// Requires GITHUB_TOKEN credential to be defined in the environment
+
+// https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#create-a-release
+
+def call(Map args = [ repo: '', release: '', target_ref: '' ]) {
+
+  // if repo not given, assume own repo
+  String ownerRepo = arg.repo ?: gitOwnerRepo()
+
+  String release = args.release ?: githubNextRelease()
+
+  String target_ref = args.target_ref ?: env.GIT_COMMIT
+
+  echo "Creating GitHub repo '$ownerRepo' release: $release"
+
+  String jsonOutput = sh(
+    returnStdout: true,
+    label: 'Create GitHub Release',
+    script: """
+      # do not set -x here as it'll expose your GITHUB_TOKEN in the logs
+      set -eu
+      set -o pipefail 2>/dev/null || :
+      curl \
+        -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer \$GITHUB_TOKEN"\
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/$ownerRepo/releases \
+        -d '{
+              "tag_name":"$release",
+              "target_commitish": "$target_ref",
+              "name": "$release",
+              "body": "Description of the release",
+              "draft": false,
+              "prerelease": false,
+              "generate_release_notes": false
+            }'
+    """
+  )
+
+
+}
