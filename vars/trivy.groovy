01e76db7cc480d2248b88b73fcaa9b05ca001ae6
Hari Sekhon <harisekhon@gmail.com>
2023-05-10 23:16:56 +0100

    updated trivy.groovy

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -19,19 +19,34 @@
 
 // Security scanner
 
-// Pass the target as the first arg, otherwise requires DOCKER_IMAGE and DOCKER_TAG to be set in environment{} section of Jenkinsfile pipeline
+// Pass list of Docker image:tag targets (will attempt to infer from environment DOCKER_IMAGE and DOCKER_TAG otherwise
 
-def call(target="$DOCKER_IMAGE:$DOCKER_TAG", timeoutMinutes=10){
+def call(targets=[], fail=true, timeoutMinutes=10){
   label 'Trivy'
+  if(!targets){
+    if(env.DOCKER_IMAGE){
+      String tag = 'latest'
+      if(env.DOCKER_TAG){
+        tag = env.DOCKER_TAG
+      }
+      targets = ["$DOCKER_IMAGE:$tag"]
+    } else {
+      error "No targets passed to trivy() function and no \$DOCKER_IMAGE / \$DOCKER_TAG environment variable found"
+    }
+  }
   container('trivy') {
     timeout(time: timeoutMinuntes, unit: 'MINUTES') {
       ansiColor('xterm') {
-        withEnv(["TARGET=$target"]){
-          // informational to see all issues
-          sh ' trivy image --no-progress "$TARGET" '
+        for(target in targets){
+          withEnv(["TARGET=$target"]){
+            echo "Trivy scanning image '$TARGET' - informational only to see all issues"
+            sh ' trivy image --no-progress "$TARGET" '
 
-          // fail the pipeline if any of the issues are High / Critical
-          sh ' trivy image --no-progress --exit-code 1 --severity HIGH,CRITICAL "$TARGET" '
+            if(fail){
+              echo "Trivy scanning image '$TARGET' for HIGH/CRITICAL vulnerabilities - will fail if any are detected"
+              sh ' trivy image --no-progress --exit-code 1 --severity HIGH,CRITICAL "$TARGET" '
+            }
+          }
         }
       }
     }
