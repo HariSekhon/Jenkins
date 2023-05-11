5c1df524b6fd292c04f1b6dc1e389d1db4b0757a
Hari Sekhon <harisekhon@gmail.com>
2023-05-11 03:35:39 +0100

    added trivy.groovy

---
+++ vars/trivy.groovy
@@ -0,0 +1,54 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2022-01-06 17:19:11 +0000 (Thu, 06 Jan 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                                   T r i v y
+// ========================================================================== //
+
+// Security scanner
+
+// For Usage see adjacent trivyScanDockerImages.groovy
+//
+// Requires:
+//
+// - a Jenkins agent with Docker available locally see https://github.com/HariSekhon/Kubernetes-configs/blob/master/jenkins/base/jenkins-agent-pod.yaml
+//
+// - if pulling from Google Container Registry - gcpSetupApplicationCredentials.groovy (adjacent) run in the trivy container before calling this function eg.
+//
+//      withCredentials([string(credentialsId: 'jenkins-gcp-serviceaccount-key', variable: 'GCP_SERVICEACCOUNT_KEY')]) {
+//        container('trivy'){
+//          gcpSetupApplicationCredentials()
+//        }
+//      }
+//
+//
+// Usage:
+//
+
+def call (args='') {
+  label 'Trivy'
+  container('trivy') {
+    timeout(time: timeoutMinutes, unit: 'MINUTES') {
+      ansiColor('xterm') {
+        withEnv (["ARGS=$args"]) {
+          sh (
+            label: "Trivy",
+            script: ' trivy $ARGS '
+          )
+        }
+      }
+    }
+  }
+}
