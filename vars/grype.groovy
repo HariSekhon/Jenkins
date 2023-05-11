abd5eaab0be714b014c3684a67a8c1c01d4349d3
Hari Sekhon <harisekhon@gmail.com>
2023-05-11 21:42:52 +0100

    updated grype.groovy

--- vars/grype.groovy
+++ vars/grype.groovy
@@ -52,6 +52,8 @@
 def call (targetList=[], fail=true, timeoutMinutes=10) {
   label 'Grype'
   if (targetList) {
+    //  ! targetList instanceof List   does not work and
+    //    targetList !instanceof List  is only available in Groovy 3
     if (targetList instanceof List == false) {
       error "non-list passed as first arg to grype() function"
     }
