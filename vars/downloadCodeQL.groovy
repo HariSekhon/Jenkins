e1ef37bd1427b73ec57a298bb13c96b2241fa052
Hari Sekhon <harisekhon@gmail.com>
2023-05-11 20:51:58 +0100

    updated downloadCodeQL.groovy

--- vars/downloadCodeQL.groovy
+++ vars/downloadCodeQL.groovy
@@ -58,7 +58,7 @@ def call() {
         rm -fr -- ./codeql
         # the -- breaks the tar command which attempts to take it literally on GCloud SDK container
         #tar xvzf -- ./"$tarball"
-        tar xvzf ./"$tarball" -C ~/bin/
+        tar xvzf ./"$tarball"
         echo
 
         unalias rm >/dev/null 2>/dev/null || :
