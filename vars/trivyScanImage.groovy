de1ddfd800503d3b81706578750d47cdd15e6ce9
Hari Sekhon <harisekhon@gmail.com>
2023-05-11 03:35:41 +0100

    updated trivyScanImage.groovy

--- vars/trivyScanImage.groovy
+++ vars/trivyScanImage.groovy
@@ -14,25 +14,14 @@
 //
 
 // ========================================================================== //
-//                                   T r i v y
+//                T r i v y   S c a n   D o c k e r   I m a g e s
 // ========================================================================== //
 
 // Security scanner
 
 // Pass list of Docker image:tag targets (will attempt to infer from environment DOCKER_IMAGE and DOCKER_TAG otherwise)
 
-// Requires:
-//
-// - a Jenkins agent with Docker available locally see https://github.com/HariSekhon/Kubernetes-configs/blob/master/jenkins/base/jenkins-agent-pod.yaml
-//
-// - if pulling from Google Container Registry - gcpSetupApplicationCredentials.groovy (adjacent) run in the trivy container before calling this function eg.
-//
-//      withCredentials([string(credentialsId: 'jenkins-gcp-serviceaccount-key', variable: 'GCP_SERVICEACCOUNT_KEY')]) {
-//        container('trivy'){
-//          gcpSetupApplicationCredentials()
-//        }
-//      }
-//
+// For requirements see adjacent trivy.groovy
 //
 // Usage:
 //
@@ -58,22 +47,13 @@ def call (Map args = [targets=[], fail=true, timeoutMinutes=10]) {
   }
   container('trivy') {
     timeout(time: timeoutMinutes, unit: 'MINUTES') {
-      ansiColor('xterm') {
-        for (target in targets) {
-          withEnv (["TARGET=$target"]) {
-            echo "Trivy scanning image '$TARGET' - informational only to see all issues"
-            sh (
-              label: "Trivy",
-              script: ' trivy image --no-progress "$TARGET" '
-            )
-
-            if (fail) {
-              echo "Trivy scanning image '$TARGET' for HIGH/CRITICAL vulnerabilities - will fail if any are detected"
-              sh (
-                label: "Trivy",
-                script: ' trivy image --no-progress --exit-code 1 --severity HIGH,CRITICAL "$TARGET" '
-              )
-            }
+      for (target in targets) {
+        withEnv (["TARGET=$target"]) {
+          echo "Trivy scanning image '$TARGET' - informational only to see all issues"
+          trivy('image --no-progress "$TARGET"')
+          if (fail) {
+            echo "Trivy scanning image '$TARGET' for HIGH/CRITICAL vulnerabilities - will fail if any are detected"
+            trivy('image --no-progress --exit-code 1 --severity HIGH,CRITICAL "$TARGET"')
           }
         }
       }
