928ad9e374547990612f4e272edd2f6c8b5859c8
Hari Sekhon <harisekhon@gmail.com>
2023-05-13 06:06:39 +0100

    updated trivyImages.groovy

--- vars/trivyImages.groovy
+++ vars/trivyImages.groovy
@@ -77,6 +77,21 @@ def call (imageList=[], severity='HIGH,CRITICAL', timeoutMinutes=30) {
         echo "Trivy scanning image '$IMAGE' - informational only to see all issues"
         trivy("image --no-progress --timeout '${timeoutMinutes}m' '$IMAGE'")
 
+        String filename = 'trivy-image-' + image.split(':')[0].replace('/', '_') + '.html'
+        echo "Trivy generate report for image '$IMAGE'"
+        sh('mkdir -p reports')
+        trivy("image --no-progress --timeout '${timeoutMinutes}m' '$IMAGE' --format template --template '@trivy-html.tpl' -o 'reports/$filename'")
+        echo "Publish HTML report to Jenkins for image '$IMAGE'"
+        publishHTML target : [
+          allowMissing: true,
+          alwaysLinkToLastBuild: true,
+          keepAll: true,
+          reportDir: 'reports',
+          reportFiles: "$filename",
+          reportName: "Trivy Image Scan '$IMAGE'",
+          reportTitles: "Trivy Image Scan '$IMAGE'"
+        ]
+
         echo "Trivy scanning image '$IMAGE' for severity '$SEVERITY' vulnerabilities only - will fail if any are detected"
         trivy("image --no-progress --timeout '${timeoutMinutes}m' --exit-code 1 --severity '$SEVERITY' '$IMAGE'")
       }
