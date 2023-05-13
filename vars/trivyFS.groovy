cb5ec0f124355ebcbd5b805db30419cb672d20b3
Hari Sekhon <harisekhon@gmail.com>
2023-05-13 06:06:38 +0100

    updated trivyFS.groovy

--- vars/trivyFS.groovy
+++ vars/trivyFS.groovy
@@ -52,6 +52,20 @@ def call (dir='.', severity='HIGH,CRITICAL', timeoutMinutes=10) {
       echo "Trivy scanning dir '$DIR' - informational only to see all issues"
       trivy("fs '$DIR' --no-progress --timeout '${timeoutMinutes}m'")
 
+      echo "Trivy generate report for dir '$DIR'"
+      sh('mkdir -p reports')
+      trivy("fs '$DIR' --no-progress --timeout '${timeoutMinutes}m' --format template --template '@trivy-html.tpl' -o 'reports/trivy-fs.html'")
+      echo "Publish HTML report to Jenkins"
+      publishHTML target : [
+        allowMissing: true,
+        alwaysLinkToLastBuild: true,
+        keepAll: true,
+        reportDir: 'reports',
+        reportFiles: 'trivy-fs.html',
+        reportName: 'Trivy FS Scan',
+        reportTitles: 'Trivy FS Scan'
+      ]
+
       echo "Trivy scanning dir '$DIR' for severity '$SEVERITY' vulnerabilities only - will fail if any are detected"
       trivy("fs '$DIR' --no-progress --timeout '${timeoutMinutes}m' --exit-code 1 --severity '$SEVERITY'")
     }
