563fbffb867435844f065fbf5dc55c1ea895206a
Hari Sekhon <harisekhon@gmail.com>
2023-05-17 01:32:31 +0100

    added sonarScanner.groovy

---
+++ vars/sonarScanner.groovy
@@ -0,0 +1,38 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2022-01-06 17:19:11 +0000 (Thu, 06 Jan 2022)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                         S o n a r   S c a n n e r
+// ========================================================================== //
+
+// CLI Scanner for SonarQube
+//
+// Repo should have a sonar-project.properties at its root for the project specific settings
+
+def call (timeoutMinutes=30) {
+  label 'Sonar Scanner'
+  // let caller decide if wrapping this in a container('grype') or using downloadGrype.groovy to save RAM
+  //container('sonar-scanner') {
+    timeout (time: timeoutMinutes, unit: 'MINUTES') {
+      ansiColor('xterm') {
+        echo "Sonar Scanner"
+        sh (
+          label: "Sonar Scanner",
+          script: "sonar-scanner"
+        )
+      }
+    }
+  //}
+}
