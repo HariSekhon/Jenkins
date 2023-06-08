1b9df35f6c6af179b18ca18e01980b88ea0ecec6
Hari Sekhon <harisekhon@gmail.com>
2023-06-08 23:40:18 +0100

    added jenkinsJobList.groovy

---
+++ vars/jenkinsJobList.groovy
@@ -0,0 +1,25 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2023-06-08 23:36:19 +0100 (Thu, 08 Jun 2023)
+//
+//  vim:ts=2:sts=2:sw=2:et
+//
+//  https://github.com/HariSekhon/Jenkins
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+//
+//  https://www.linkedin.com/in/HariSekhon
+//
+
+// ========================================================================== //
+//                        J e n k i n s   J o b   L i s t
+// ========================================================================== //
+
+// Returns a list of strings of jenkins job names
+
+def call() {
+  List<String> jobs = jenkins.model.Jenkins.instance.items.findAll().collect { it.name }
+  return jobs
+}
