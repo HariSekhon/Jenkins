dd4d739136c1baad9a14bf7212fd3b476328f626
Hari Sekhon <harisekhon@gmail.com>
2023-06-30 21:36:14 +0100

    updated approval.groovy

--- vars/approval.groovy
+++ vars/approval.groovy
@@ -21,19 +21,20 @@
 
 // Usage:
 //
-//    approval(submitter: 'platform-engineering@mycompany.com,Deployers', timeout: 10)
+//             // submitter can be in group email or name format
+//    approval(submitter: 'platform-engineering@mycompany.com,ApproversGroup', timeout: 10)
 //
-// or better to DRY between pipelines:
+// or better to DRY between pipelines use a global variable:
 //
-//    approval(submitter: "$DEPLOYERS", timeout: 10)
+//    approval(submitter: "$APPROVERS", timeout: 10)
 //
 // set to 2 hours instead of default 60 minutes - these values are those supported by the standard Jenkins timeout() function:
 //
-//    approval(submitter: "$DEPLOYERS", timeout: 2, timeoutUnits: 'HOURS')
+//    approval(submitter: "$APPROVERS", timeout: 2, timeoutUnits: 'HOURS')
 //
-// then configure $DEPLOYERS environment variable at the global Jenkins level:
+// then configure $APPROVERS environment variable at the global Jenkins level:
 //
-//    Manage Jenkins -> Configure System -> Global properties -> Environment Variables -> Add -> DEPLOYERS
+//    Manage Jenkins -> Configure System -> Global properties -> Environment Variables -> Add -> APPROVERS
 //
 // submitter = comma separated list of users/groups by name or email address that are permitted to authorize
 // ok        = what the ok button should say, defaults to 'Proceed' if empty/unspecified
@@ -53,7 +54,7 @@ def call (Map args = [submitter:'', timeout:60, timeoutUnits: 'MINUTES', ok:''])
 This prompt will time out in ${timeout} ${timeoutUnits.toLowerCase()}""",
       ok: args.ok,
       // only allow people in these 2 groups to approve before proceeeding eg. to production deployment - this list can now be provided as an argument
-      //submitter: "platform-engineering@mydomain.co.uk,Deployers"
+      //submitter: "platform-engineering@mydomain.co.uk,ApproversGroup"
       submitter: args.submitter
     )
   }
